10a8ae8c9b7bf836ec79f573c9d28d73e97f4498
Khairul Bashar <khairul.bashar@drivewyze.com>
2021-05-24 15:45:14 -0600

    fix Fragment dependancy

--- app/src/main/java/com/bashar/salatreminder/FragmentAboutList.java
+++ app/src/main/java/com/bashar/salatreminder/FragmentAboutList.java
@@ -3,7 +3,7 @@ package com.bashar.salatreminder;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
-import androidx.core.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/com/bashar/salatreminder/FragmentHelp.java
+++ app/src/main/java/com/bashar/salatreminder/FragmentHelp.java
@@ -3,7 +3,8 @@ package com.bashar.salatreminder;
 
 import android.os.Bundle;
 import androidx.annotation.Nullable;
-import androidx.core.app.Fragment;
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
